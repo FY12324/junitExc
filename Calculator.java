329320ce8b014f8242b2209e2e74a6a4ae3eabb1
FY <73886074+FY12324@users.noreply.github.com>
2022-05-24 14:24:51 +0800

    Calculator
    A calculator test.

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package lib;
+
+public class Calculator {
+		private static int result;
+		public void add(int n) {
+			result = result + n;
+		}
+		public void substract(int n) {
+			result = n- result;
+		}
+		 public void multiply(int n){
+			 result = result * n;
+		}         
+		public void divide(int n) {
+			result =n/result;
+		}
+		public void clear() {
+			result = 1;
+		}
+		public int getResult() {
+			return result;
+		}
+	}
+
---
+++ SubstractTest.java
@@ -0,0 +1,35 @@
+package lib;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SubstractTest {
+	Calculator calculator=new Calculator();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		calculator.substract(2);
+		int actual=calculator.getResult();
+		assertEquals(2, actual);
+		calculator.clear();
+	}
+	
+	@Test
+	public void test1() {
+		calculator.substract(6);
+		int actual=calculator.getResult();
+		assertEquals(5, actual);
+	}
+
+}
